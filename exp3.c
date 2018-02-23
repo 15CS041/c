60f589cbf1b9235b271fdd378d8760cac50bf008
15CS041 <rajaudhayaap1998@gmail.com>
2018-02-23 14:22:40 +0530

    Create exp3.c

---
+++ exp3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+printf("enter the char")
+scanf("%c",&ch);
+if(ch='a'||ch='e'||ch='i'||ch='o'||ch='u')
+{
+printf("consonant");
+}
+else
+{
+printf("vowels");
+}
+return 0;
+}
+
