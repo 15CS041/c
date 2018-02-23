f8086e21a0f1648216d3a0836e7256403813af71
15CS041 <rajaudhayaap1998@gmail.com>
2018-02-23 13:50:43 +0530

    Create exp1.c

---
+++ exp1.c
@@ -0,0 +1,20 @@
+#include<studio.h>
+int main()
+{
+int a;
+printf("Enter the a");
+scanf("%d",&a);
+if(a==0)
+{
+printf("positive");
+}
+else if(a>0)
+{
+printf("nagative");
+}
+else
+{
+printf("Zero");
+}
+return 0;
+}
